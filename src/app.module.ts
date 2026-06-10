dbe6659137bc1ecd00f97dc8405a88736a1cc7c6
Cespedes Brian <briancdevelop@gmail.com>
2026-06-10 13:18:25 -0300

    fix: supabase-postgres-best-practices

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
