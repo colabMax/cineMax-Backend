34b19a4cab10e7563b703c46f6c32da78fbf6033
Cespedes Brian <briancdevelop@gmail.com>
2026-06-20 09:43:06 -0300

    feat(user): add validation rules for create user dto

---
+++ src/user/dto/create-user.dto/create-user.dto.ts
@@ -0,0 +1,62 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MaxLength(20, {
+    message: 'El nombre debe tener un máximo de 20 caracteres',
+  })
+  @IsNotEmpty({
+    message: 'El nombre no puede estar vacío',
+  })
+  name!: string;
+
+
+
+  @IsEmail({}, {
+    message: 'El email no tiene un formato válido',
+  })
+  @IsNotEmpty({
+    message: 'El email no puede estar vacío',
+  })
+  @MaxLength(50, {
+    message: 'El email debe tener un máximo de 50 caracteres',
+  })
+  email!: string;
+
+
+
+
+  @IsString()
+  @IsNotEmpty({
+    message: 'La contraseña no puede estar vacía',
+  })
+  
+  @MinLength(8, {
+    message: 'La contraseña debe tener un mínimo de 8 caracteres',
+  })
+
+  @Matches(/[a-z]/, {
+    message: 'La contraseña debe contener al menos una letra minúscula',
+  })
+
+  @Matches(/[A-Z]/, {
+    message: 'La contraseña debe contener al menos una letra mayúscula',
+  })
+
+  @Matches(/\d/, {
+    message: 'La contraseña debe contener al menos un número',
+  })
+
+  @Matches(/[@$!%*?&]/, {
+    message:
+      'La contraseña debe contener al menos un carácter especial (@$!%*?&)',
+  })
+  password!: string;
+}
