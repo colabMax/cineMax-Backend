d29c1a738c5383384a719df4eff07dfd18782c42
Cespedes Brian <briancdevelop@gmail.com>
2026-06-20 09:50:46 -0300

    feat(user): implement user listing with Prisma

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './../prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async findAll() {
+    return await this.prisma.user.findMany();
+  }
+}
